aa965477f955e978fca31b3acfe196466d26646e
oneangrytrader <oneangrytrader@gmail.com>
2021-02-23 05:18:52 +0000

    .

---
+++ src/main/java/com/angrytrader/ib/api/client/Main.java
@@ -0,0 +1,9 @@
+package com.angrytrader.ib.api.client;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("com.angrytrader.ib.api.client.Main.main()");
+        
+    }    
+}
